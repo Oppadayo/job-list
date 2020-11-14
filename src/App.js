423540b53bcc64b5fde03a94861d2011d7e814dd
Oppadayo <paula.correapcs@gmail.com>
2020-11-14 04:18:50 -0300

    add data and style desktop/mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import data from './assets/data.json'
+import JobBoardComponent from './assets/components/JobBoardComponent'
 
 function App() {
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() => {
+    setJobs(data)
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hi, Lorena</h1>
+      <header className="bg-teal-500 mb-12">
+        <img src="/images/bg-header-desktop.svg" alt="bg-img" />
+      </header>      
+      {
+        jobs.length === 0 ? (
+          <p>Trabalhos carregando...</p>
+        ) : (
+          jobs.map((job) => 
+            <JobBoardComponent job={job} key={job.id} />
+          )
+        )
+      }
+      
     </div>
   );
 }
